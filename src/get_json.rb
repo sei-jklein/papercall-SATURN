727053d9c180d73c2d1ec783f65f7480983e9fa2
jklein <jklein@sei.cmu.edu>
2020-02-19 17:19:11 -0500

    Removed personally-identifying info from json dump

--- src/get_json.rb
+++ src/get_json.rb
@@ -14,4 +14,9 @@ Papercall.fetch(:from_papercall,
                 :waitlist,
                 :declined)
 
-File.write('/saturn/saturn-' + Date.today.to_s + '.json', Papercall.all.to_json)
+abbr_submissions = Array.new # Abbreviated submissions, sanitize identifying information
+Papercall.all.each do |submission|
+    abbr_submissions << submission['talk']
+end
+
+File.write('/saturn/saturn-' + Date.today.to_s + '.json', abbr_submissions.to_json)
